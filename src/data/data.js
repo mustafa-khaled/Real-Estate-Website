e257dadbb1a9498f88273fce2ee03da1c778bfe7
Mustafa <mostafakhaled0787314@gmail.com>
2023-11-25 12:56:01 +0200

    singles data

--- src/data/data.js
+++ src/data/data.js
@@ -15,6 +15,59 @@ import phone from "../Assets/phone.png";
 import email from "../Assets/email.png";
 import location from "../Assets/location-1.png";
 
+import villa1 from "../Assets/villa1.jpg";
+import villa2 from "../Assets/villa2.jpg";
+import villa3 from "../Assets/villa3.jpeg";
+import villa4 from "../Assets/villa4.jpeg";
+import villa5 from "../Assets/villa5.jpg";
+
+import apartment1 from "../Assets/apartemn1.jpg";
+import apartment2 from "../Assets/apartemn2.jpg";
+import apartment3 from "../Assets/apartemn3.jpg";
+import apartment4 from "../Assets/apartemn4.jpeg";
+import apartment5 from "../Assets/apartemn5.jpg";
+import apartment6 from "../Assets/apartemn6.jpeg";
+
+import hotelApartment1 from "../Assets/tower1.jpeg";
+import hotelApartment2 from "../Assets/towe2.jpg";
+import hotelApartment3 from "../Assets/towe3.jpg";
+import hotelApartment4 from "../Assets/tower4.jpg";
+import hotelApartment5 from "../Assets/tower5.jpg";
+
+import commercial1 from "../Assets/commercial1.jpg";
+import commercial2 from "../Assets/commercial2.jpeg";
+import commercial3 from "../Assets/commercial3.jpg";
+import commercial4 from "../Assets/commercial4.jpg";
+import commercial5 from "../Assets/commercial5.jpg";
+import commercial6 from "../Assets/commercial6.jpeg";
+
+import administrative1 from "../Assets/administrative1.jpg";
+import administrative2 from "../Assets/administrative2.jpg";
+import administrative3 from "../Assets/administrative3.jpg";
+import administrative4 from "../Assets/administrative4.jpeg";
+import administrative5 from "../Assets/administrative5.jpg";
+import administrative6 from "../Assets/administrative6.jpg";
+
+import medical from "../Assets/medical1.jpg";
+import medica2 from "../Assets/medical2.png";
+import medica3 from "../Assets/medical3.png";
+import medica4 from "../Assets/medical4.jpg";
+import medica5 from "../Assets/medical5.jpg";
+import medica6 from "../Assets/medical6.jpg";
+
+import pharmacy1 from "../Assets/pharmacies1.jpg";
+import pharmacy2 from "../Assets/pharmacies2.jpg";
+import pharmacy3 from "../Assets/pharmacies3.jpg";
+import pharmacy4 from "../Assets/pharmacies4.jpg";
+import pharmacy5 from "../Assets/pharmacies5.jpg";
+import pharmacy6 from "../Assets/pharmacies6.jpg";
+
+import newCairo1 from "../Assets/newCairo1.jpg";
+import newCairo2 from "../Assets/newCairo2.jpg";
+import newCairo3 from "../Assets/newCairo3.jpg";
+import newCairo4 from "../Assets/newCairo4.jpeg";
+import newCairo5 from "../Assets/newCairo5.jpeg";
+
 import {
   FaFacebookSquare,
   FaInstagram,
@@ -176,4 +229,380 @@ export const contactInfoData = [
   { id: 1, title: "+20 1151747091", image: phone },
   { id: 2, title: "mustafakhaledinfo@gmail.com", image: email },
   { id: 3, title: "Cairo, Egypt", image: location },
+];
+
+export const villaData = [
+  {
+    id: 1,
+    image: villa1,
+    title: "Midtown Villa",
+    discount: "10%",
+    Installment: 6,
+  },
+
+  {
+    id: 2,
+    image: villa2,
+    title: "Midtown Solo",
+    discount: "10%",
+    Installment: 7,
+  },
+
+  {
+    id: 3,
+    image: villa3,
+    title: "Midtown Sky",
+    discount: "10%",
+    Installment: 7,
+  },
+
+  {
+    id: 4,
+    image: villa4,
+    title: "IL Bosco",
+    discount: "5%",
+    Installment: 9,
+  },
+
+  {
+    id: 5,
+    image: villa5,
+    title: "Vinci",
+    discount: "5%",
+    Installment: 9,
+  },
+];
+
+export const apartmentsData = [
+  {
+    id: 1,
+    image: apartment1,
+    title: "Midtown Sky",
+    discount: "10%",
+    Installment: 7,
+  },
+
+  {
+    id: 2,
+    image: apartment2,
+    title: "Midtown Condo",
+    discount: "10%",
+    Installment: 7,
+  },
+
+  {
+    id: 3,
+    image: apartment3,
+    title: "IL Bosco",
+    discount: "5%",
+    Installment: 9,
+  },
+
+  {
+    id: 4,
+    image: apartment4,
+    title: "Vinci",
+    discount: "5%",
+    Installment: 9,
+  },
+
+  {
+    id: 5,
+    image: apartment5,
+    title: "Serrano",
+    discount: "15%",
+    Installment: 6,
+  },
+
+  {
+    id: 6,
+    image: apartment6,
+    title: "Atika",
+    discount: "10%",
+    Installment: 8,
+  },
+];
+
+export const hotelApartmentsData = [
+  {
+    id: 1,
+    image: hotelApartment1,
+    title: "Central Iconic Tower",
+    discount: "10%",
+    Installment: 15,
+  },
+
+  {
+    id: 2,
+    image: hotelApartment2,
+    title: "6Ixty Iconic Tower",
+    discount: "10%",
+    Installment: 7,
+  },
+
+  {
+    id: 3,
+    image: hotelApartment3,
+    title: "East Tower",
+    discount: "10%",
+    Installment: 7,
+  },
+
+  {
+    id: 4,
+    image: hotelApartment4,
+    title: "Double Two Tower",
+    discount: "10%",
+    Installment: 6,
+  },
+
+  {
+    id: 5,
+    image: hotelApartment5,
+    title: "Rain Tower",
+    discount: "10%",
+    Installment: 5,
+  },
+];
+
+export const commercialData = [
+  {
+    id: 1,
+    image: commercial1,
+    title: "Pyramids Mall",
+    discount: "30%",
+    Installment: 5,
+  },
+
+  {
+    id: 2,
+    image: commercial2,
+    title: "Lafayette Mall",
+    discount: "10%",
+    Installment: 6,
+  },
+
+  {
+    id: 3,
+    image: commercial3,
+    title: "Unitower",
+    discount: "10%",
+    Installment: 6,
+  },
+
+  {
+    id: 4,
+    image: commercial4,
+    title: "Loft Plaza",
+    discount: "10%",
+    Installment: 6,
+  },
+
+  {
+    id: 5,
+    image: commercial5,
+    title: "Senet",
+    discount: "10%",
+    Installment: 6,
+  },
+
+  {
+    id: 6,
+    image: commercial6,
+    title: "Pint 9 Mall",
+    discount: "10%",
+    Installment: 8,
+  },
+];
+
+export const administrativeData = [
+  {
+    id: 1,
+    image: administrative1,
+    title: "Solano Mall",
+    discount: "10%",
+    Installment: 6,
+  },
+
+  {
+    id: 2,
+    image: administrative2,
+    title: "Laval",
+    discount: "10%",
+    Installment: 6,
+  },
+
+  {
+    id: 3,
+    image: administrative3,
+    title: "Dorado",
+    discount: "10%",
+    Installment: 6,
+  },
+
+  {
+    id: 4,
+    image: administrative4,
+    title: "Ronza Tower",
+    discount: "15%",
+    Installment: 7,
+  },
+
+  {
+    id: 5,
+    image: administrative5,
+    title: "Onyx Tower",
+    discount: "10%",
+    Installment: 5,
+  },
+
+  {
+    id: 6,
+    image: administrative6,
+    title: "Mizar Tower",
+    discount: "10%",
+    Installment: 5,
+  },
+];
+
+export const medicalData = [
+  {
+    id: 1,
+    image: medical,
+    title: "Laval",
+    discount: "10%",
+    Installment: 6,
+  },
+
+  {
+    id: 2,
+    image: medica2,
+    title: "Cloud",
+    discount: "15%",
+    Installment: 8,
+  },
+
+  {
+    id: 3,
+    image: medica3,
+    title: "Senator",
+    discount: "10%",
+    Installment: 7,
+  },
+
+  {
+    id: 4,
+    image: medica4,
+    title: "Ronza Tower",
+    discount: "10%",
+    Installment: 6,
+  },
+
+  {
+    id: 5,
+    image: medica5,
+    title: "Senet",
+    discount: "10%",
+    Installment: 6,
+  },
+
+  {
+    id: 6,
+    image: medica6,
+    title: "Evora Mall",
+    discount: "16%",
+    Installment: 9,
+  },
+];
+
+export const pharmaciesData = [
+  {
+    id: 1,
+    image: pharmacy1,
+    title: "Lafayette Mall",
+    discount: "10%",
+    Installment: 6,
+  },
+
+  {
+    id: 2,
+    image: pharmacy2,
+    title: "Loft Mall",
+    discount: "15%",
+    Installment: 6,
+  },
+
+  {
+    id: 3,
+    image: pharmacy3,
+    title: "UIN Tower",
+    discount: "10%",
+    Installment: 8,
+  },
+
+  {
+    id: 4,
+    image: pharmacy4,
+    title: "Point 11",
+    discount: "10%",
+    Installment: 9,
+  },
+
+  {
+    id: 5,
+    image: pharmacy5,
+    title: "Park Point Mall",
+    discount: "10%",
+    Installment: 6,
+  },
+
+  {
+    id: 6,
+    image: pharmacy6,
+    title: "Pagoda Complex",
+    discount: "15%",
+    Installment: 5,
+  },
+];
+
+export const newCairoData = [
+  {
+    id: 1,
+    image: newCairo1,
+    title: "The Vibe",
+    discount: "10%",
+    Installment: 6,
+  },
+
+  {
+    id: 2,
+    image: newCairo2,
+    title: "City Hall",
+    discount: "10%",
+    Installment: 7,
+  },
+
+  {
+    id: 3,
+    image: newCairo3,
+    title: "Axle",
+    discount: "10%",
+    Installment: 7,
+  },
+
+  {
+    id: 4,
+    image: newCairo4,
+    title: "In Town Mall",
+    discount: "15%",
+    Installment: 8,
+  },
+
+  {
+    id: 5,
+    image: newCairo5,
+    title: "Cinco",
+    discount: "6%",
+    Installment: 9,
+  },
 ];
